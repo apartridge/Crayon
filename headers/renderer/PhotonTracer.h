f84ace4845c1985fafc487b92e5fb2c1f0a6d504
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2012-05-27 00:41:23 -0700

    Added functions to PhotonTracer

--- headers/renderer/PhotonTracer.h
+++ headers/renderer/PhotonTracer.h
@@ -3,18 +3,27 @@
 
 #include "renderer/PhotonMap.h"
 #include "geometry/Scene.h"
+#include "lights/Light.h"
 
 class PhotonTracer
 {
 public:
-    // Shoot out photons from the light sources in the scene, and record in photon map
-    void traceScene(const Scene& scene, unsigned long numberOfPhotons);
+    // Transmit photons from the light sources in the scene, and record in global photon map.
+    // TODO: Can also add photon map for caustics only. Need to only tracce photons through specular materals.
+    void traceScene(const Scene& scene, int numberOfPhotons);
 
     // Return photon map generated from traceScene
     PhotonMap* getPhotonMap();
 
 private:
-    PhotonMap* photonMap;
+    const Scene* _scene;
+    PhotonMap* _photonMap;
+
+    // Generate photons from a light source
+    void traceLight(const Light& light, int numberOfPhotons);
+
+    // Trace a photon through the scene
+    void PhotonTracer::tracePhoton(Ray& ray);
 
 };
 
--- src/renderer/PhotonTracer.cpp
+++ src/renderer/PhotonTracer.cpp
@@ -1,9 +1,57 @@
 #include "renderer/PhotonTracer.h"
 
+#include "sysutils/Random.h"
 
-void PhotonTracer::traceScene(const Scene& scene, unsigned long numberOfPhotons)
+
+void PhotonTracer::traceScene(const Scene& scene, int numberOfPhotons)
 {
+    _scene = &scene;
+    _photonMap = new PhotonMap(numberOfPhotons);
 
+    const Lights *lightlist = scene.lights();
+    const int numberOfLights = lightlist->size();
+
+    Lights::const_iterator it;
+
+    for (it = lightlist->begin(); it != lightlist->end(); ++it)
+    {
+        const Light* l = *it;
+        traceLight(*l, numberOfPhotons / (float)numberOfLights);
+    }
+
+    // Balance KD-tree to optimize search
+    _photonMap->balance();
+}
+
+void PhotonTracer::traceLight(const Light& light, int numberOfPhotons)
+{
+    for (int i = 0; i < numberOfPhotons; ++i)
+    {
+        Vector3 pos = light.getPosition();
+
+        // Generate random vector (assume pointlight)
+        Vector3 dir;
+        do {
+            dir.x = Random::uniformRand();
+            dir.y = Random::uniformRand();
+            dir.z = Random::uniformRand();
+        } while (dot(dir, dir) > 1);
+        
+        Ray ray;
+        ray.o = pos;
+        ray.d = dir.normalized();
+        ray.mediumOfTravel.indexOfRefraction = 1.0;
+
+        tracePhoton(ray);
+    }
+}
+
+void PhotonTracer::tracePhoton(Ray& ray)
+{
+    HitInfo hit;
+    if (!_scene->trace(hit, ray, epsilon))
+        return;
+    
 
 
 }
@@ -11,7 +59,7 @@ void PhotonTracer::traceScene(const Scene& scene, unsigned long numberOfPhotons)
 
 PhotonMap* PhotonTracer::getPhotonMap()
 {
-    return this->photonMap;
+    return this->_photonMap;
 }
 
 
