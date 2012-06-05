2d01651c71b93e9970a52f9a6fe05e9a6afbf8c3
Stian Pedersen <stian.pedersen@gmail.com>
2012-06-05 03:40:40 -0700

    New world.obj.

--- src/finalsproject.cpp
+++ src/finalsproject.cpp
@@ -75,13 +75,12 @@ void makeFinalScene()
         g_scene->addObject(trg);*/
 
         PointLight * light = new PointLight;
-        light->setPosition(Vector3(88, 32, 0)); // Vector3(89.2, 11, 10)*3
+        light->setPosition(Vector3(88, 32, 18)); // Vector3(89.2, 11, 10)*3
         light->setColor(Vector3(1, 1, 1));
         light->setPower(50000);
         // Set target at windows, to not waste photons
         LightTarget* target = new LightTarget(Vector3(15.81, 5.40, 9.3), 15);
         light->setTarget(target);
-
         g_scene->addLight(light);
     }
     
@@ -210,8 +209,8 @@ void makeFinalScene()
 	mesh->connectNameToMaterial("ChessWhite", white);
 	mesh->connectNameToMaterial("ChessBlack", black);
 
-    Material* glassChessPieceWhite = new Glass(1.4, Vector3(1, 1, 1));
-    Material* glassChessPieceBlack = new Glass(1.4, Vector3(0.7, 1, 0.7));
+    Material* glassChessPieceWhite = new Glass(1.5, Vector3(1, 1, 1));
+    Material* glassChessPieceBlack = new Glass(1.5, Vector3(0.6, 1, 0.6));
     mesh->connectNameToMaterial("ChessPiecesOne", glassChessPieceWhite);
     mesh->connectNameToMaterial("ChessPiecesTwo", glassChessPieceBlack);
 
